00ac3203c2bcb932ddfc8f1aacea5c20d39d1b62
Diego Marquez <diegom75@nycstudents.net>
2025-05-29 14:37:13 +0000

    changes to script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let siteDisplay = document.getElementsById("siteDisplay");
+
+function showHome() {
+    siteDisplay.innerHTML = ""
+    siteDisplay.innerHTML = `
+    <p>test<p>
+    `
+}
+
+function showAbout() {
+    siteDisplay.innerHTML = ""
+    siteDisplay.innerHTML = `
+    <p>test2<p>
+    `
+}
